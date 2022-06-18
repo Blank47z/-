8fa353a8f0cb7675706b8623245552d02bd3f835
Blank47z <107739103+Blank47z@users.noreply.github.com>
2022-06-18 11:24:11 +0800

    Add files via upload

---
+++ data.source.js
@@ -0,0 +1,597 @@
+import React from 'react';
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0 l4ihh2tz7e-editor_css' },
+  textWrapper: { className: 'banner0-text-wrapper l4ihhfzwr2-editor_css' },
+  title: {
+    className: 'banner0-title l4igrhzm6os-editor_css',
+    children: (
+      <span>
+        <p>我的世界如初宝可梦</p>
+      </span>
+    ),
+  },
+  content: {
+    className: 'banner0-content l4ihh8rtvcb-editor_css',
+    children: (
+      <span>
+        <p>为广大mc玩家提供真正有趣的宝可梦服务器</p>
+        <p>
+          <br />
+        </p>
+      </span>
+    ),
+  },
+  button: {
+    className: 'banner0-button l4igtzw4xoh-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <span>
+                      <p>服务器官方群892213100</p>
+                      <p>
+                        （所有人都在等待你的到来<span>）</span>
+                      </p>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+};
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0 l4imb5r35po-editor_css' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>服务器介绍</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper l4imbsita47-editor_css',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>丰富的新手福利</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>
+                    简单新手任务，5v忍娃，6v准神，洛奇亚，幸运方块大师球等进服免费送
+                  </p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>方便的菜单化操作</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>按T键掌握全局，无需繁琐指令</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>高概率神异兽刷新</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>80%三小时刷出神兽，Blank异世界一天遭遇12个异兽</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Teams10DataSource = {
+  wrapper: { className: 'home-page-wrapper teams1-wrapper' },
+  page: { className: 'home-page teams1 l4imack2wip-editor_css' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>服务器成员</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            { name: 'image', className: 'teams1-image', children: '' },
+            {
+              name: 'title',
+              className: 'teams1-title',
+              children: (
+                <span>
+                  <p>Shanks</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>如梦宝可梦服务器服主</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content l4imf2erm2i-editor_css',
+              children: (
+                <span>
+                  <span>
+                    <span>
+                      <p>
+                        <b>
+                          游手好闲，朴实亲和，善待萌新，有时需要什么道具可以找他要，一次两次会给你（也不能太离谱），可能会突然tp到你身边（也不知道干什么）总之是个好人awa
+                        </b>
+                      </p>
+                    </span>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            { name: 'image', className: 'teams1-image', children: '' },
+            {
+              name: 'title',
+              className: 'teams1-title',
+              children: (
+                <span>
+                  <p>mocai</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>服务器搭建者</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content l4imebnimsg-editor_css',
+              children: (
+                <span>
+                  <span>
+                    <span>
+                      <p>
+                        <b>
+                          掌管着服务器运行以及各种各样的bug，亲和朴实，善待萌新，手中的权力并不低于Shanks，仍是个值得相处的朋友，会解答各样问题，有耐心不急躁。
+                        </b>
+                      </p>
+                    </span>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            { name: 'image', className: 'teams1-image', children: '' },
+            {
+              name: 'title',
+              className: 'teams1-title',
+              children: (
+                <span>
+                  <p>Blank_world</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>网站搭建者</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content l4imfabsxsa-editor_css',
+              children: (
+                <span>
+                  <span>
+                    <span>
+                      <p>
+                        <b>
+                          不咋懂宝可梦的憨批，我就占个位置不行嘛？不行嘛！！！！
+                        </b>
+                      </p>
+                    </span>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Pricing10DataSource = {
+  wrapper: { className: 'home-page-wrapper pricing1-wrapper' },
+  page: { className: 'home-page pricing1 l4im9utdoz8-editor_css' },
+  OverPack: { playScale: 0.3, className: 'pricing1-content-wrapper' },
+  titleWrapper: {
+    className: 'pricing1-title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <p>经济实惠的氪金服务（更多详情请前往如初宝可梦官方群）热门</p>
+            </span>
+          </span>
+        ),
+        className: 'pricing1-title-h1',
+      },
+    ],
+  },
+  block: {
+    className: 'pricing1-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'pricing1-block',
+        md: 8,
+        xs: 24,
+        children: {
+          wrapper: { className: 'pricing1-block-box ' },
+          topWrapper: { className: 'pricing1-top-wrapper' },
+          name: {
+            className: 'pricing1-name',
+            children: (
+              <span>
+                <p>首充礼包</p>
+              </span>
+            ),
+          },
+          money: {
+            className: 'pricing1-money',
+            children: (
+              <span>
+                <p>¥6</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'pricing1-content',
+            children: (
+              <span>
+                <p>金币3000</p>
+                <p>首充3狗(炎帝雷公水军)自选个</p>
+              </span>
+            ),
+          },
+          line: { className: 'pricing1-line' },
+          buttonWrapper: {
+            className: 'pricing1-button-wrapper',
+            children: {
+              a: {
+                className: 'pricing1-button',
+                href: '#https://jq.qq.com/?_wv=1027&k=ULTd5XiW',
+                children: (
+                  <span>
+                    <span>
+                      <span>
+                        <span>
+                          <span>
+                            <span>
+                              <span>
+                                <span>
+                                  <p>前往qq群寻找腐竹购买</p>
+                                </span>
+                              </span>
+                            </span>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </span>
+                ),
+                target: '_blank',
+              },
+            },
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'pricing1-block',
+        md: 8,
+        xs: 24,
+        children: {
+          wrapper: { className: 'pricing1-block-box active' },
+          topWrapper: { className: 'pricing1-top-wrapper' },
+          name: {
+            className: 'pricing1-name',
+            children: (
+              <span>
+                <p>v2</p>
+                <p>
+                  <br />
+                </p>
+              </span>
+            ),
+          },
+          money: {
+            className: 'pricing1-money',
+            children: (
+              <span>
+                <p>¥100</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'pricing1-content',
+            children: (
+              <span>
+                <p>金币3w</p>
+                <p>大师球1个 幸运方块3个</p>
+                <p>vip2</p>
+                <p>三云自选一个 一次改性格</p>
+              </span>
+            ),
+          },
+          line: { className: 'pricing1-line' },
+          buttonWrapper: {
+            className: 'pricing1-button-wrapper',
+            children: {
+              a: {
+                className: 'pricing1-button',
+                href: '#https://jq.qq.com/?_wv=1027&k=ULTd5XiW',
+                children: (
+                  <span>
+                    <p>前往qq群寻找腐竹购买</p>
+                  </span>
+                ),
+                target: '_blank',
+              },
+            },
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'pricing1-block',
+        md: 8,
+        xs: 24,
+        children: {
+          wrapper: { className: 'pricing1-block-box ' },
+          topWrapper: { className: 'pricing1-top-wrapper' },
+          name: {
+            className: 'pricing1-name',
+            children: (
+              <span>
+                <p>至尊v6</p>
+              </span>
+            ),
+          },
+          money: {
+            className: 'pricing1-money',
+            children: (
+              <span>
+                <p>¥600</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'pricing1-content',
+            children: (
+              <span>
+                <span>
+                  <p>金币15w</p>
+                  <p>幸运方块32个 大师球32个</p>
+                  <p>剑狗盾狗或者x神y神自选一个3v大小性格 3个精灵皮肤&nbsp;</p>
+                </span>
+              </span>
+            ),
+          },
+          line: { className: 'pricing1-line' },
+          buttonWrapper: {
+            className: 'pricing1-button-wrapper',
+            children: {
+              a: {
+                className: 'pricing1-button',
+                href: '#https://jq.qq.com/?_wv=1027&k=ULTd5XiW',
+                children: (
+                  <span>
+                    <span>
+                      <p>前往qq群寻找腐竹购买</p>
+                    </span>
+                  </span>
+                ),
+                target: '_blank',
+              },
+            },
+          },
+        },
+      },
+    ],
+  },
+};
+export const Content130DataSource = {
+  OverPack: {
+    className: 'home-page-wrapper content13-wrapper l4ik8h31yef-editor_css',
+    playScale: 0.3,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children:
+          'https://gw.alipayobjects.com/zos/rmsportal/PiqyziYmvbgAudYfhuBr.svg',
+        className: 'title-image',
+      },
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>我的世界如初宝可梦服务器</p>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <p>永远不倒，陪您到老</p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+      {
+        name: 'content2',
+        children: (
+          <span>
+            <p>等待，您的到来</p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright l4in7fbsqjk-editor_css',
+    children: (
+      <span>
+        ©2018 <a href="https://motion.ant.design">Ant Motion</a> All Rights
+        Reserved
+      </span>
+    ),
+  },
+};
